6e921f60cfa7abea5bdacc4feb8f18726d28e80d
Gregorio <gregoriocarranza@hotmail.com>
2022-05-20 18:41:16 -0300

    principio de form

--- src/Pages/Contacto/index.jsx
+++ src/Pages/Contacto/index.jsx
@@ -1,12 +1,13 @@
 import "./index.css";
 
 import { Fragment } from "react";
+import Form from "../../Components/Form/Form.jsx"
 
 function Contacto() {
   return (
     <Fragment>
       <h1>Hola soy Contacto</h1>
-      {/* <Carrousel /> */}
+      <Form />
     </Fragment>
   );
 }
